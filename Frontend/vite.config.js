7273c14fd407568ab8826996789edd0c0c804921
awaisanwarcode <muhammadayub56122@gmail.com>
2025-10-04 07:12:48 +0500

    Updated the frontend

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
+
+
+
+
+// export default defineConfig({
+//   base: './',   // 👈 Add this line
+//   build: {
+//     outDir: 'dist'
+//   }
+// })
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './'
 })
